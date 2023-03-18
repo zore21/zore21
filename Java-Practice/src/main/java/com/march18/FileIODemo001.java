79aaa62569dee93209281a4fd1f29d517b650e53
prathamesh zore <pmzore10@gmail.com>
2023-03-18 15:08:14 +0530

    commited

--- Java-Practice/src/main/java/com/march18/FileIODemo001.java
+++ Java-Practice/src/main/java/com/march18/FileIODemo001.java
@@ -1,5 +1,6 @@
 package com.march18;
 
+import javax.swing.*;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 
@@ -7,7 +8,11 @@ public class FileIODemo001 {
     public static void main(String[] args) {
         try {
             FileOutputStream flieout = new FileOutputStream("abc.txt");
-            String string = "Hello world!! How are you ?";
+//            String string = "Hello world!! How are you ?";
+
+            String string = JOpti   onPane.showInputDialog(null,
+                    "Enter message here ");
+
             byte[] bytes = string.getBytes();
             flieout.write(bytes);
 
