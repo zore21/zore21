5f040460b8178497c581f7910dcd90645e0f7c57
prathamesh zore <prathameshzore@benchmarkit.solutions>
2023-03-18 13:43:00 +0530

    java-practice is pushed

--- Java-Practice/src/main/java/com/march18/FileIODemo001.java
+++ Java-Practice/src/main/java/com/march18/FileIODemo001.java
@@ -1,15 +1,25 @@
 package com.march18;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 
 public class FileIODemo001 {
     public static void main(String[] args) {
         try {
-            FileOutputStream flieout =
-                    new FileOutputStream("abc.txt");
+            FileOutputStream flieout = new FileOutputStream("abc.txt");
             String string = "Hello world!! How are you ?";
             byte[] bytes = string.getBytes();
             flieout.write(bytes);
+
+            FileInputStream finput = new FileInputStream("abc.txt");
+            while (true){
+                int i = finput.read();
+                if(i == -1)
+                    break;
+
+                System.out.print((char)i);
+            }
+
         }
         catch (Exception e){
             e.printStackTrace();
