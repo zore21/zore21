651f62ccc72d28ce0c2548c93ed64e6a0f4e2414
prathamesh zore <prathameshzore@benchmarkit.solutions>
2023-03-18 13:36:21 +0530

    java code to write a file

--- Java-Practice/src/main/java/com/march18/FileIODemo001.java
+++ Java-Practice/src/main/java/com/march18/FileIODemo001.java
@@ -1,4 +1,18 @@
 package com.march18;
 
+import java.io.FileOutputStream;
+
 public class FileIODemo001 {
+    public static void main(String[] args) {
+        try {
+            FileOutputStream flieout =
+                    new FileOutputStream("abc.txt");
+            String string = "Hello world!! How are you ?";
+            byte[] bytes = string.getBytes();
+            flieout.write(bytes);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
