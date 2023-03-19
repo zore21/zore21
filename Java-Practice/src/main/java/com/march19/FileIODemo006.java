4fe044c3c8e3d4a142422f2c81eb31406798e876
prathamesh zore <pmzore10@gmail.com>
2023-03-19 13:44:49 +0530

    FileIODemo with PrintStream

--- Java-Practice/src/main/java/com/march19/FileIODemo006.java
+++ Java-Practice/src/main/java/com/march19/FileIODemo006.java
@@ -8,8 +8,22 @@ import java.io.PrintStream;
 public class FileIODemo006 {
     public static void main(String[] args) throws FileNotFoundException {
         PrintStream printStream =
-                new PrintStream(new FileOutputStream("file3.txt"));
+                new PrintStream(new FileOutputStream("file4.txt"));
 
-        printStream.println("Hello how are you?");
+        // by default out is the static final varible
+        // of System class whose type is PrintStream
+        // by default it gives output on console
+        // but this can change by using setOut()
+        // method we can provide where should the
+        // output prints
+
+
+        System.setOut(printStream);
+        System.out.println("Hi Hello this is PrintStream demo " +
+                "this is printing on a different file not on " +
+                "console");
+
+
+
     }
 }
